990b4c735b38798e609dc54b8df519a15b388fcc
Sam Pawson <samuel.pawson@gmail.com>
2020-03-10 19:10:59 +0000

    Added search form validation

--- static/js/form.js
+++ static/js/form.js
@@ -2,21 +2,25 @@ var region = document.getElementById("region-select");
 var editCourse = document.getElementById("edit-course-form");
 var addCourse = document.getElementById("add-course-form");
 var errorElement = document.getElementById("region-error");
-
-
+var regionSelect = document.getElementById("region");
+var textSearch = document.getElementById("review-title");
+var minRating = document.getElementById("min-rating");
+var searchForm = document.getElementById("search");
+var searchError= document.getElementById("search-error")
 
 if(editCourse != null){
-    editCourse.addEventListener("submit", validation, false);
+    editCourse.addEventListener("submit", courseValidation, false);
 }
-
 if(addCourse != null){
-    addCourse.addEventListener("submit", validation, false);
+    addCourse.addEventListener("submit", courseValidation, false);
+}
+if(searchForm != null){
+    searchForm.addEventListener("submit", searchValidation, false);
 }
 
 
 
-
-function validation(event){
+function courseValidation(event){
     let messages = [];
     if (region.value == "" || region.value == null){
         messages.push("You must select a region");
@@ -26,8 +30,20 @@ function validation(event){
         event.preventDefault();
         errorElement.innerHTML = messages.join()
     }
+    
+}
 
 
+function searchValidation(event){
+    let messages = [];
+    if ((regionSelect.value == "" || region.value == null) && textSearch.value == "" && (minRating.value == 0 || minRating.value == null)){
+        messages.push("You must use at least one filter");
+    }
+
+    if (messages.length > 0){
+        event.preventDefault();
+        searchError.innerHTML = messages.join()
+    }
     
 }
 
